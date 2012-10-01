36eaf6f304cfafc5b2774ae96011c477df3c5675
Andy Gregorowicz <andy@mitre.org>
2012-10-01 16:11:38 -0400

    Patching TemplateHelper because it will look for ERB files in health-data-standards. Now it will look in this gem.

--- lib/qrda_generator/export/cat_1.rb
+++ lib/qrda_generator/export/cat_1.rb
@@ -3,6 +3,13 @@ module QrdaGenerator
     module Cat1
       include HealthDataStandards::Export::TemplateHelper
 
+      # Hack to override the template_root in Health Data Standards,
+      # which will only reference templates in the Health Data Standard
+      # gem
+      def template_root
+        File.join(File.dirname(__FILE__), '..', '..', '..', 'templates')
+      end
+
       def export(patient)
         self.template_format = "cat1"
         render(:template => 'show', :locals => {:patient => patient})
