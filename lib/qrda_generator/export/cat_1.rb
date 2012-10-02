f1e387b29173cebf93185b32542988b1c36396b6
Andy Gregorowicz <andy@mitre.org>
2012-10-02 17:26:45 -0400

    Implementing the reporting parameters section of the QRDA Cat 1 Document export

--- lib/qrda_generator/export/cat_1.rb
+++ lib/qrda_generator/export/cat_1.rb
@@ -3,9 +3,10 @@ module QrdaGenerator
     module Cat1
       include HealthDataStandards::Export::TemplateHelper
 
-      def export(patient, measures)
+      def export(patient, measures, start_date, end_date)
         self.template_format = "cat1"
-        render(:template => 'show', :locals => {:patient => patient, :measures => measures})
+        render(:template => 'show', :locals => {:patient => patient, :measures => measures, 
+                                                :start_date => start_date, :end_date => end_date})
       end
 
       extend self
--- test/unit/export/cat_1_test.rb
+++ test/unit/export/cat_1_test.rb
@@ -6,11 +6,13 @@ class Cat1Test < MiniTest::Unit::TestCase
     collection_fixtures('measures')
     @patient = Record.first
     @measures = []
+    @start_date = Time.now.years_ago(1)
+    @end_date = Time.now
 
     Mongoid.session(:default)['measures'].find.each do |measure|
       @measures << HQMF::Document.from_json(measure)
     end
-    @qrda_xml = QrdaGenerator::Export::Cat1.export(@patient, @measures)
+    @qrda_xml = QrdaGenerator::Export::Cat1.export(@patient, @measures, @start_date, @end_date)
     @doc = Nokogiri::XML(@qrda_xml)
     @doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
     
@@ -29,5 +31,13 @@ class Cat1Test < MiniTest::Unit::TestCase
       measure_entry.at_xpath('./cda:organizer/cda:reference/cda:externalDocument/cda:setId[@root="B5FEE67F-F5C4-4C73-9D58-1941F9729539"]').present?
     end
     assert measure
+  end
+
+  def test_reporting_parameters_section_export
+    effective_time = @doc.at_xpath('//cda:section[cda:templateId/@root="2.16.840.1.113883.10.20.17.2.1"]/cda:entry/cda:act/cda:effectiveTime')
+    assert effective_time
+
+    assert_equal @start_date.to_formatted_s(:number), effective_time.at_xpath('./cda:low')['value']
+    assert_equal @end_date.to_formatted_s(:number), effective_time.at_xpath('./cda:high')['value']
   end
 end
