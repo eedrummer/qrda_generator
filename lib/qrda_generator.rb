0f34f37d19cf3e483ecc987e3cc32926a827cd45
Andy Gregorowicz <andy@mitre.org>
2012-10-08 13:54:57 -0400

    Starting to add code to work with HQMF to QRDA oid translation

--- lib/qrda_generator.rb
+++ lib/qrda_generator.rb
@@ -6,3 +6,4 @@ require_relative 'qrda_generator/ext/hds_freedom_patch'
 
 require_relative 'qrda_generator/export/value_set_manager'
 require_relative 'qrda_generator/export/cat_1'
+require_relative 'qrda_generator/export/entry_template_resolver'
