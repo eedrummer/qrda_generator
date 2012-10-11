e97b185d46b3f2dee2a0ba000e874cff603da2c3
Andy Gregorowicz <andy@mitre.org>
2012-10-11 09:12:47 -0400

    Switching from a class to a module to be consistent with other view generation related code

--- lib/qrda_generator/export/entry_template_resolver.rb
+++ lib/qrda_generator/export/entry_template_resolver.rb
@@ -1,7 +1,7 @@
 module QrdaGenerator
   module Export
-    class EntryTemplateResolver
-      def self.hqmf_qrda_oid_map
+    module EntryTemplateResolver
+      def hqmf_qrda_oid_map
         if @hqmf_qrda_oid_map.blank?
           template_id_file = File.expand_path('../hqmf-qrda-oids.json', __FILE__)
           @hqmf_qrda_oid_map = JSON.parse(File.read(template_id_file))  
@@ -9,13 +9,15 @@ module QrdaGenerator
         @hqmf_qrda_oid_map
       end
 
-      def self.qrda_oid_exist?(oid)
+      def qrda_oid_exist?(oid)
         hqmf_qrda_oid_map.any? {|map_tuple| map_tuple['qrda_oid'] == oid}
       end
 
-      def self.qrda_oid_for_hqmf_oid(hqmf_oid)
+      def qrda_oid_for_hqmf_oid(hqmf_oid)
         hqmf_qrda_oid_map.find {|map_tuple| map_tuple['hqmf_oid'] == hqmf_oid }['qrda_oid'] 
       end
+
+      extend self
     end
   end
 end
