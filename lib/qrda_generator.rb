0bb4cabbcc47b20bcb0aafe41e300258f84685e1
Andy Gregorowicz <andy@mitre.org>
2012-11-28 13:05:27 -0500

    Removing the monkey patch to TemplateHelper in HDS. We just override the method in the Cat1 exporter class now.

--- lib/qrda_generator.rb
+++ lib/qrda_generator.rb
@@ -1,9 +1,6 @@
 require 'health-data-standards'
 require 'hqmf-parser'
 
-# FREEDOM!!!!!
-require_relative 'qrda_generator/ext/hds_freedom_patch'
-
 require_relative 'qrda_generator/export/value_set_manager'
 require_relative 'qrda_generator/export/cat_1'
 require_relative 'qrda_generator/export/entry_template_resolver'
--- lib/qrda_generator/export/cat_1.rb
+++ lib/qrda_generator/export/cat_1.rb
@@ -4,6 +4,13 @@ module QrdaGenerator
       include HealthDataStandards::Export::TemplateHelper
       include HealthDataStandards::Util
 
+      # Hack to override the template_root in Health Data Standards,
+      # which will only reference templates in the Health Data Standard
+      # gem
+      def template_root
+        File.join(File.dirname(__FILE__), '..', '..', '..', 'templates')
+      end
+
       def export(patient, measures, start_date, end_date)
         self.template_format = "cat1"
         render(:template => 'show', :locals => {:patient => patient, :measures => measures, 
