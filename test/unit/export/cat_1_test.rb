231d3bcdbc2a688ebdd99e00a3ccf0ac38f38183
Andy Gregorowicz <andy@mitre.org>
2012-12-07 11:06:53 -0500

    Fixing the test to bring it in line with the proper places for measure ids in the QRDA

--- test/unit/export/cat_1_test.rb
+++ test/unit/export/cat_1_test.rb
@@ -44,7 +44,7 @@ class Cat1Test < MiniTest::Unit::TestCase
     measure_entries = @doc.xpath('//cda:section[cda:templateId/@root="2.16.840.1.113883.10.20.24.2.3"]/cda:entry')
     assert_equal 3, measure_entries.size
     measure = measure_entries.find do |measure_entry|
-      measure_entry.at_xpath('./cda:organizer/cda:reference/cda:externalDocument/cda:id[@root="0002"]').present? &&
+      measure_entry.at_xpath('./cda:organizer/cda:reference/cda:externalDocument/cda:id[@extension="0002"]').present? &&
       measure_entry.at_xpath('./cda:organizer/cda:reference/cda:externalDocument/cda:setId[@root="B5FEE67F-F5C4-4C73-9D58-1941F9729539"]').present?
     end
     assert measure
